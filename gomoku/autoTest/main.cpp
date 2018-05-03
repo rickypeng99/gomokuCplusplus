48be5bb86f99a5b84255c50a7c9d3d6c80b08325
rickypeng99 <rickypeng99@gmail.com>
2018-05-02 21:56:49 -0500

    updated making sounds

---
+++ gomoku/autoTest/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  autoTest
+//
+//  Created by Ricky Peng on 4/30/18.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
